cc55573e4581fc729c9100e053dafc711d199fd3
Chelsea Rodrigues <84131870+chelsea2311@users.noreply.github.com>
2021-08-27 13:48:00 +0530

    Add files via upload

---
+++ Python basic assignment no.3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  PYTHON BASIC THEORY ASSIGNMENT NO 3
+
+# Q1. Functions are advantageous to have in our program because it is convinient to call a function rather than repetatively writing same lines of codes again and again. Also, we can use the same function multiple times in a program. i.e functions increases reusuability of code.
+
+# Q2. The code in the function runs when the function is called using the function name and not when the function is defined using keyword.
+
+# Q3. Functions are created using the def keyword followed by the function name and then closed brackets with colon at the end. Then we write the statement thaat the function needs to execute when called in the later part of the code. Syntax for creating a function named test for printing a statement: 
+# def test():
+# print ("this is how you define a function")
+
+# Q4. A function is used to achieve a particular result while function call is used to achieve that task using the function. So basically, using a function to do a particular task at any point in the program is called a function call. 
+
+# Q5. A variable's scope is the range of the script where it is visible. Global scope means it is visible in every function and local scope means the variables declared inside a function and it has a limited scope. There is only one global scope in python, whereas there can be many local scopes depending upon the function in program.  
+
+# Q6. A return value is used to end the execution of the function call and it returns the result to the caller. Yes, return values only use expressions, but if a return statement is without any expression , then a special value none is returned.
+
+# Q7. A return value is used to end the execution of the function call and it returns the result to the caller. Yes, return values only use expressions, but if a return statement is without any expression, then a special value none is returned.
+
+# Q8. If a function does not have a return statement, control automatically returns to the calling function after the last statement of the called function is executed. Function ends after execution of the body and no value is returned.
+
+# Q9. If we want to refer to a global variable in a function, we can use the global keyword to declare which variables are global in our program.
+
+# Q10. The None keyword is used to define a null value. It has its own datatype that is called the Nonetype.
+
+# Q11. The import statement "import areallyoutpetnamederic" will import module named "areallyourpetsnamederic"
+
+# Q12. This function can be called as spam.bacon() .
+
+# Q13. For preventing the program from crashing, we can include null checks, changing execution order , etc. Error handling can be used to notify the user of why the error occured and gracefully exit the process that caused the error.
+
+# Q14. In pythin, the try block is used to check some code for errors. The code inside the try block will execute when there is no error in the program.The except block(except clause) lets you handle the error in python.
